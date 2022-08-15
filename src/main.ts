9ea98a853564b285e4c0a302e12fc8987a0f706c
Leonardo Collatto <collatto99@gmail.com>
2022-08-15 17:18:15 -0300

    port fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
